9a6d1815a1542263b0e5faaf2605ded88729cda5
NoahOldfield <noah@simula.no>
2025-01-16 23:40:42 +0100

    Added model SimpleNN

--- mnist_classification/mnist_classifier.py
+++ mnist_classification/mnist_classifier.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import torch.nn as nn
 
 # fetch mnist dataset
 mnist_dataset = torchvision.datasets.MNIST(
@@ -15,4 +16,23 @@ train_loader = torch.utils.data.DataLoader(
     batch_size = 64,
     shuffle = True
     )
+
+class SimpleNN(torch.nn.Module):
+
+    def __init__(self):
+
+        super(SimpleNN, self).__init__()
+        self.initial_layer  = nn.Linear(28*28, 128)
+        self.hidden_layer_1 = nn.Linear(128, 64)
+        self.hidden_layer_2 = nn.Linear(64, 10)
+    
+    def forward(self):
+
+        x = x.view(-1, 28 * 28)
+
+        # apply layers and activations
+        x = nn.ReLU()(self.initial_layer(x))
+        x = nn.ReLU()(self.hidden_layer_1(x))
+        x = self.hidden_layer_2(x)  # No activation on final layer
+
 
